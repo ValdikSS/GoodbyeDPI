5494be72ba374b688d87d6ecb5024d71cd1803ff
ValdikSS <iam@valdikss.org.ru>
2021-12-30 03:46:51 +0300

    GoodbyeDPI v0.2.0

--- src/goodbyedpi.c
+++ src/goodbyedpi.c
@@ -23,7 +23,7 @@
 // My mingw installation does not load inet_pton definition for some reason
 WINSOCK_API_LINKAGE INT WSAAPI inet_pton(INT Family, LPCSTR pStringBuf, PVOID pAddr);
 
-#define GOODBYEDPI_VERSION "v0.1.8"
+#define GOODBYEDPI_VERSION "v0.2.0"
 
 #define die() do { sleep(20); exit(EXIT_FAILURE); } while (0)
 
