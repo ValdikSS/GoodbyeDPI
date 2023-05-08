55679c46bbcaad75146556c0684063b22f103eed
jimraynor2470 <j.raynor2470@protonmail.ch>
2023-05-08 10:35:39 +0900

    Add whitelist option

--- src/blackwhitelist.h
+++ src/blackwhitelist.h
@@ -1,2 +1,4 @@
-int blackwhitelist_load_list(const char *filename);
-int blackwhitelist_check_hostname(const char *host_addr, size_t host_len);
+int blackwhitelist_load_blacklist(const char *filename);
+int blackwhitelist_load_whitelist(const char *filename);
+int blackwhitelist_check_hostname_blacklist(const char *host_addr, size_t host_len);
+int blackwhitelist_check_hostname_whitelist(const char *host_addr, size_t host_len);
