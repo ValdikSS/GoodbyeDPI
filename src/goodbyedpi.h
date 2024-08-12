50fb908630591dabbb492cbd1e42d09e30203a7d
EgorWeders <157705588+EgorWeders@users.noreply.github.com>
2024-08-12 04:43:01 +0000

    Added getenv to debug macro

--- src/goodbyedpi.h
+++ src/goodbyedpi.h
@@ -1,11 +1,11 @@
 #define HOST_MAXLEN 253
 #define MAX_PACKET_SIZE 9016
 
-#ifndef DEBUG
-#define debug(...) do {} while (0)
-#else
-#define debug(...) printf(__VA_ARGS__)
-#endif
-
+// #ifndef DEBUG
+// #define debug(...) do {} while (0)
+// #else
+// #define debug(...) printf(__VA_ARGS__)
+// #endif
+#define debug(...) if (getenv("DEBUG_GDPI")!=NULL) printf(__VA_ARGS__)
 int main(int argc, char *argv[]);
 void deinit_all();
