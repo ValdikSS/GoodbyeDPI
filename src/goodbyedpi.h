78b787ba39cb5ca3e8b030ba6cc91166b2aa1a23
EgorWeders <157705588+EgorWeders@users.noreply.github.com>
2024-09-18 03:41:45 +0000

    Added global pointer

--- src/goodbyedpi.h
+++ src/goodbyedpi.h
@@ -1,8 +1,11 @@
 #define HOST_MAXLEN 253
 #define MAX_PACKET_SIZE 9016
-
+extern int (*debugPrint)(char const *const _Format, ...);
 #ifndef DEBUG
-#define debug(...) do {} while (0)
+#define debug(...)                                                             \
+	if (debugPrint) {                                                            \
+		debugPrint(__VA_ARGS__);                                                   \
+	}
 #else
 #define debug(...) printf(__VA_ARGS__)
 #endif
